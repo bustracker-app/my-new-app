c146035134c27c7443479a7a096449b57ba112e0
Play store Console <database.gov.in@gmail.com>
2026-02-11 12:58:25 +0000

    feat: Configure project for Netlify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-    ],
-  },
+  output: 'standalone',
 };
 
 export default nextConfig;
