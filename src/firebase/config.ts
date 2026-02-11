a33fc1e8c3d7e6c15976c843150adfeba771d0f1
Play store Console <database.gov.in@gmail.com>
2026-02-11 06:28:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8892733844-6d7b0",
+  "appId": "1:478035933535:web:41c898bb109fb027541eea",
+  "apiKey": "AIzaSyBM2sMPGz4Db-qLetUlmYaiN0maOH0wo3c",
+  "authDomain": "studio-8892733844-6d7b0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "478035933535"
+};
