104783f75d680b5e9f353508c800f8a2c318f30a
Play store Console <database.gov.in@gmail.com>
2026-02-11 09:29:24 +0000

    Ok creat it suitable for netlify (everything will be building) and give

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
 };
